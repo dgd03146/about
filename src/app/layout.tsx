ac7b3eaf85e7ed9c83e3be75b566ece40e6178d6
dgd03146 <ibory1220@naver.com>
2023-08-08 16:57:37 +0100

    chore: Set up toast #1

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import Toast from '@/components/common/toast/toast'
+import Toast from '@/components/common/toast'
 import ThemeProviders from './themeProviders'
 
 const inter = Inter({ subsets: ['latin'] })
