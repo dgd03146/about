61297a262ba37d849822c936037b76f78a50c3ec
dgd03146 <ibory1220@naver.com>
2023-08-09 12:28:22 +0100

    chore: Set up vanila extract #1

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from 'next'
+import { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import ThemeProviders from './themeProviders'
 import Toast from '@/components/common/toast'
