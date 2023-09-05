e4314baff4f6387a95f676f9b85069bb0ff48cb3
dgd03146 <ibory1220@naver.com>
2023-09-05 16:15:38 +0100

    design: Create center component design-system #2

---
+++ src/design/components/Center/Center.tsx
@@ -0,0 +1,27 @@
+import type * as Polymorphic from '@radix-ui/react-polymorphic'
+import React from 'react'
+
+import { Box, BoxProps } from '../Box'
+
+interface Props {}
+
+type PolymorphicCenter = Polymorphic.ForwardRefComponent<
+  Polymorphic.IntrinsicElement<typeof Box>,
+  BoxProps & Props
+>
+
+export type CenterProps = Polymorphic.OwnProps<PolymorphicCenter>
+
+export const Center = React.forwardRef((props, ref) => {
+  return (
+    <Box
+      ref={ref}
+      display="flex"
+      alignItems="center"
+      justifyContent="center"
+      {...props}
+    />
+  )
+}) as PolymorphicCenter
+
+Center.displayName = 'Center'
